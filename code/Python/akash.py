8f95c2f4f1906b05e73c3d6e216791222a649d34
Its me Rzky <mail.rizkytegar@gmail.com>
2022-10-12 14:01:05 +0700

    Update akash.py

--- code/Python/akash.py
+++ code/Python/akash.py
@@ -1,21 +1,13 @@
-
-
 import math
 import os
 import random
 import re
 import sys
 
-
-
 def staircase(n):
-   
     
     for i in range(1,n+1):
-        print(" "*(n-i)+"#"*i)
-       
-        
-    
+        print(" "*(n-i)+"#"*i)   
 
 if __name__ == '__main__':
     n = int(input().strip())
